76bae70aa64f52a3fc4c13c248ff391e497d0998
Thanh Duong <dhthanhqa@gmail.com>
2021-09-25 08:46:27 +0700

    update b2

--- js/bai02.js
+++ js/bai02.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    $("div.items > div.item:nth-child(even)").addClass("animate__animated animate__slideInLeft")
-    $("div.items > div.item:nth-child(odd)").addClass("animate__animated animate__slideInRight")
+    $("div.items > div.item:nth-child(even)").addClass("wow animate__slideInLeft")
+    $("div.items > div.item:nth-child(odd)").addClass("wow animate__slideInRight")
     $("#btnAdd").click(function() {
        var content = $("#txtContent").val() 
        var date = $("#txtDate").val()
@@ -34,4 +34,20 @@ $(document).ready(function() {
         if (confirm("Chac chan xoa khong?") == true)
             $(this).parent().parent().remove()
     })
+
+    
+
+    $("div.images figure").addClass("c4-izmir c4-border-cc-3 c4-image-zoom-out c4-gradient-bottom-rightc4-izmir c4-border-right c4-image-pan-left c4-gradient-top wow animate__flipInY")
+    $("div.images figure > figcaption").addClass("c4-layout-top-right")
+
+    wow = new WOW(
+        {
+            boxClass:     'wow',      // default
+            animateClass: 'animate__animated', // default
+            offset:       0,          // default
+            mobile:       true,       // default
+            live:         true        // default
+          }
+      )
+      wow.init();
 })
